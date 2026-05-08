217a4bd36cb9b1ec1de141840c584bc0cdd0a358
marika340 <mari3511n@gmail.com>
2026-05-08 13:30:21 -0400

    updated the servo to be 180 degrees and made WallFollow.h and cpp to be the same as competition

--- tof_modular/WallFollow.h
+++ tof_modular/WallFollow.h
@@ -46,9 +46,9 @@ private:
 
   // Geometry / tuning
   float _desiredWallDist  = 67.0f; //CHANGED FROM 60 ORIGINAL, 75 COULD NOT HANDLE CORNERS WELL, 65 sometimes goes to ramp and sometimes avoids ramp, 55 avoids ramp
-  float _frontStopDist    = 300.0f;  // increased so robot starts turning sooner
+  float _frontStopDist    = 280.0f;  // increased so robot starts turning sooner, wall attack: 280, original: 300
   float _wallLostDist     = 600.0f;
-  float _turnClearDist    = 300.0f; // MUST stay > _frontStopDist or handleCorner() pivot loop never runs
+  float _turnClearDist    = 285.0f; // MUST stay > _frontStopDist or handleCorner() pivot loop never runs, wall attack: 285, original: 300
   float _switchMargin     = 40.0f;
   int   _baseSpeed        = 140;
   int   _minSpeed         = 80;
